901b8e1b9745d082f63644182b03e2f338784786
Harshit933 <harshitverma933@gmail.com>
2023-05-01 11:26:25 +0530

    addon(core): This commit adds the tiki dependency

--- flutter/admob/lib/home_screen.dart
+++ flutter/admob/lib/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:admob/reward_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:tiki_sdk_flutter/tiki_sdk.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -50,18 +52,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _showRewaredAd() async {
-    await _rewardedAd!.show(onUserEarnedReward: (AdWithoutView ad, RewardItem item) {
-      num amount = item.amount;
-      debugPrint('You earned this amount $amount');
+    await _rewardedAd!.show(
+        onUserEarnedReward: (AdWithoutView ad, RewardItem item) {
+      debugPrint('You earned this amount');
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const RewardScreen()));
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-/// FIXME: THIS IS FOR TEST ADS ONLY
+  ///FIXME: THIS IS FOR TEST ADS ONLY
   void loadBanner() {
     final adUnitId = 'ca-app-pub-3940256099942544/5224354917';
     final bannerAd = BannerAd(
@@ -75,6 +74,23 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    TikiSdk.config()
+        .offer
+        .reward(Image.asset("lib/assets/monkey.jpeg"))
+        .ptr("test_offer2")
+        .bullet("Learn how our ads perform ", true)
+        .use([LicenseUsecase.analytics()])
+        .tag(TitleTag.advertisingData())
+        .description("View this ad to get rewarded.")
+        .terms("lib/terms.md")
+        .duration(const Duration(days: 365))
+        .add()
+        .onAccept((p0, p1) async {
+          await _loadAd();
+          _showRewaredAd();
+        })
+        .onDecline((p0, p1) => null);
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -89,14 +105,48 @@ class _HomePageState extends State<HomePage> {
                     child: GestureDetector(
                       onTap: () async {
                         await _loadAd();
-                        await _showRewaredAd();
+                        TikiSdk.guard(
+                            "test_offer2", [LicenseUsecase.analytics()],
+                            onPass: () async {
+                          _showRewaredAd();
+                        }, onFail: (str) {
+                          TikiSdk.present(context);
+                        });
+                      },
+                      child: Container(
+                        height: 70,
+                        color: Theme.of(context).primaryColor,
+                        child: const Center(
+                          child: Text(
+                            'Click here to get rewarded! ',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () async {
+                        /// On triggering the Opt in button the OnDecline method triggers, while triggering the opt out button the OnAccept method of the offer triggers
+                        TikiSdk.settings(context);
                       },
                       child: Container(
                         height: 70,
                         color: Colors.blue,
                         child: const Center(
                           child: Text(
-                            'Click here to get rewarded! ',
+                            'Click here to open settings! ',
                             style: TextStyle(
                               color: Colors.white,
                             ),
--- flutter/admob/lib/main.dart
+++ flutter/admob/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tiki_sdk_flutter/tiki_sdk.dart';
-
 import 'home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.updateRequestConfiguration(
-      RequestConfiguration(testDeviceIds: ['A9BFA2CD58BFD0F412F9B597C5C5B380']));
-  await TikiSdk.config();
-  runApp(const MyApp());
+  MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
+      testDeviceIds: ['A9BFA2CD58BFD0F412F9B597C5C5B380']));
+  await TikiSdk.config().initialize(
+      "ee88a4a2-26e2-4361-9385-aaf7e988719f", "test_user_123",
+      onComplete: () => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +18,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      title: 'TikiSdk with admob',
+      home: HomePage(),
     );
   }
 }
