325fa92a7139b18b1681749fe789fa6f7e7116bd
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-05-04 20:10:13 -0300

    fix: flutter admob with ump simplified version

--- flutter/admob/lib/main.dart
+++ flutter/admob/lib/main.dart
@@ -1,25 +1,91 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tiki_sdk_flutter/tiki_sdk.dart';
-import 'home_screen.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
-      testDeviceIds: ['A9BFA2CD58BFD0F412F9B597C5C5B380']));
-  await TikiSdk.config().initialize(
-      "ee88a4a2-26e2-4361-9385-aaf7e988719f", "test_user_123",
-      onComplete: () => runApp(const MyApp()));
+
+  await TikiSdk.config()
+      .offer
+        .reward(Image.asset("lib/assets/monkey.jpeg"))
+        .ptr("test_offer2")
+        .bullet("Learn how our ads perform ", true)
+        .use([LicenseUsecase.analytics()])
+        .tag(TitleTag.advertisingData())
+        .description("View this ad to get rewarded.")
+        .terms("lib/terms.md")
+        .duration(const Duration(days: 365))
+        .add()
+      .onAccept((_, __) => _initAdMob())
+      .initialize("ee88a4a2-26e2-4361-9385-aaf7e988719f", "adMobTestUser3",
+        onComplete: () => runApp(const MaterialApp(
+        title: 'TIKI AdMob Example',
+        home: Scaffold(
+          body: HomeWidget()
+        )
+      )));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomeWidget extends StatelessWidget{
+  const HomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'TikiSdk with admob',
-      home: HomePage(),
+    Future.delayed(Duration.zero, () => TikiSdk.present(context));
+    return Center(
+      child: ElevatedButton(onPressed: () => _showAd() , child: const Text("Show Ad"))
     );
   }
+}
+
+void _initAdMob() {
+  ConsentDebugSettings debugSettings = ConsentDebugSettings();
+
+  ConsentRequestParameters params = ConsentRequestParameters(
+      consentDebugSettings: debugSettings);
+  ConsentInformation.instance.requestConsentInfoUpdate(
+    params,
+        () async {
+          if (await ConsentInformation.instance.isConsentFormAvailable()) {
+            _loadUmpForm();
+          }
+    },
+        (formError) => debugPrint(formError.message)
+  );
+}
+
+void _loadUmpForm() {
+  ConsentForm.loadConsentForm(
+        (ConsentForm consentForm) async {
+      var status = await ConsentInformation.instance.getConsentStatus();
+      if (status == ConsentStatus.required) {
+        consentForm.show(
+              (FormError? formError) => _loadUmpForm()
+        );
+      }
+    },
+        (formError) => debugPrint(formError.message)
+  );
+}
+
+void _showAd(){
+  MobileAds.instance.initialize();
+  final adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/1033173712'
+      : 'ca-app-pub-3940256099942544/4411468910';
+
+    InterstitialAd.load(
+        adUnitId: adUnitId,
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (ad) {
+            ad.show();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            debugPrint('InterstitialAd failed to load: $error');
+          },
+        ));
 }
