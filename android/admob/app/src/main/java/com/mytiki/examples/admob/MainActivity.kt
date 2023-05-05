e3349c16587344de2f32a2cd9a8068774bde8531
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-05-05 00:27:19 -0300

    fix: android admob basic app

--- android/admob/app/src/main/java/com/mytiki/examples/admob/MainActivity.kt
+++ android/admob/app/src/main/java/com/mytiki/examples/admob/MainActivity.kt
@@ -1,48 +1,43 @@
 package com.mytiki.examples.admob
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.OnUserEarnedRewardListener
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var rewardedAd: RewardedAd? = null
-    private val TAG = "MainActivity"
+    private var ad: InterstitialAd? = null
+    private val tag = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadAd()
         findViewById<Button>(R.id.button).setOnClickListener {
-            rewardedAd?.let { ad ->
-                ad.show(this, OnUserEarnedRewardListener { rewardItem ->
-                    Log.d(TAG, "User earned the reward.")
-                })
-            } ?: run {
-                Log.d(TAG, "The rewarded ad wasn't ready yet.")
-            }
+            ad?.show(this@MainActivity);
         }
     }
 
     private fun loadAd() {
+        MobileAds.initialize(this)
         val adRequest = AdRequest.Builder().build()
-        TikiSdk.guard()
-        RewardedAd.load(this,"ca-app-pub-3940256099942544/5224354917",
-            adRequest, object : RewardedAdLoadCallback() {
-                override fun onAdFailedToLoad(adError: LoadAdError) {
-                    Log.d(TAG, adError.toString())
-                    rewardedAd = null
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    ad = interstitialAd
                 }
 
-                override fun onAdLoaded(ad: RewardedAd) {
-                    Log.d(TAG, "Ad was loaded.")
-                    rewardedAd = ad
+                override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                    ad = null
+                    Log.d(tag, loadAdError.toString())
                 }
             })
     }
