66adde8eb20ccc6469559df1ae401c22c555d2dd
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-06-08 17:45:44 -0300

    fix(shopify): use app block settings in UI

--- shopify/extensions/theme-block/assets/tiki_embed.js
+++ shopify/extensions/theme-block/assets/tiki_embed.js
@@ -1,27 +1,56 @@
+window.Shopify.loadFeatures([
+    {
+        name: 'consent-tracking-api',
+        version: '0.1',
+    }
+],
+    function (error) {
+        if (error) {
+            console.log(error);
+        }
+    }
+);
+
+class TikiCookiePrivacy {
+    static handleAccept = (e) =>
+        window.Shopify.customerPrivacy.setTrackingConsent(true);
+
+    static handleDecline = () =>
+        window.Shopify.customerPrivacy.setTrackingConsent(false);
+
+    static get userAllowedAll() {
+        return window.Shopify.customerPrivacy.userDataCanBeSold() &&
+            window.Shopify.customerPrivacy.analyticsProcessingAllowed() &&
+            window.Shopify.customerPrivacy.preferencesProcessingAllowed() &&
+            window.Shopify.customerPrivacy.firstPartyMarketingAllowed() &&
+            window.Shopify.customerPrivacy.thirdPartyMarketingAllowed();
+    }
+}
+
 class TikiEmbed {
-    static start = async () => {
+    static start = async (settings) => {
+        let customer_id = ShopifyAnalytics.meta.page.customerId
         debugger
         TikiSdk.config()
             .theme
-            .primaryTextColor('#1C0000')
-            .secondaryTextColor('#1C000099')
-            .primaryBackgroundColor('#FFFFFF')
-            .secondaryBackgroundColor('#F6F6F6')
-            .accentColor('#00b272')
-            .fontFamily('"Space Grotesk", sans-serif')
-            .and()
+                .primaryTextColor(settings.primaryTextColor)
+                .secondaryTextColor(settings.secondaryTextColor)
+                .primaryBackgroundColor(settings.primaryBackgroundColor)
+                .secondaryBackgroundColor(settings.secondaryBackgroundColor)
+                .accentColor(settings.accentColor)
+                .fontFamily(settings.fontFamily)
+                .and()
             .offer
-            .description('Trade your IDFA (kind of like a serial # for your phone) for a discount.')
-            .reward('https://cdn.mytiki.com/assets/demo-reward.png')
-            .bullet({ text: 'Learn how our ads perform', isUsed: true })
-            .bullet({ text: 'Reach you on other platforms', isUsed: false })
-            .bullet({ text: 'Sold to other companies', isUsed: false })
-            .terms('terms.md')
-            .ptr('db2fd320-aed0-498e-af19-0be1d9630c63')
-            .tag(TikiSdk.TitleTag.deviceId())
-            .use({ usecases: [TikiSdk.LicenseUsecase.attribution()] })
-            .add()
-        let customer_id = ShopifyAnalytics.meta.page.customerId
+                .description(settings.description)
+                .reward(settings.offerImage)
+                .bullet({ text: settings.useCase1, isUsed: settings.isUsed1 })
+                .bullet({ text: settings.useCase2, isUsed: settings.isUsed2 })
+                .bullet({ text: settings.useCase3, isUsed: settings.isUsed3 })
+                .terms('terms.md')
+                .ptr(shopUrl)
+                .tag(TikiSdk.TitleTag.deviceId())
+                .use({ usecases: [TikiSdk.LicenseUsecase.attribution()] })
+                .add()
         let offer = TikiSdk.config()._offers[0];
         if (customer_id) {
             await TikiSdk.config().initialize('13d435e2-29a9-4263-a853-e2bb0e698e31', customer_id);
@@ -36,15 +65,106 @@ class TikiEmbed {
                     offer._tags,
                     offer._description,
                     offer._expiry
-                );       
+                );
             } else {
-                TikiSdk.config().onAccept(() => TikiCookiePrivacy.handleAccept() )
-                TikiSdk.config().onDecline(() => TikiCookiePrivacy.handleDecline() )
+                TikiSdk.config().onAccept(() => TikiCookiePrivacy.handleAccept())
+                TikiSdk.config().onDecline(() => TikiCookiePrivacy.handleDecline())
                 TikiSdk.present()
             }
-        }else{
+        } else {
             TikiSdkAnon.present()
         }
     }
 }
-TikiEmbed.start()
+class TikiSdkAnon {
+    static id = "tiki-offer"
+    static overlayId = "tiki-offer-overlay"
+
+    static present = () => {
+        if (document.getElementById(TikiSdkAnon.id) == null) {
+            const div = document.createElement("div");
+            div.id = TikiSdkAnon.id;
+            div.appendChild(TikiSdkAnon.createOverlay());
+            document.body.appendChild(div);
+            TikiSdkAnon.goTo('prompt');
+        }
+    }
+
+    static goTo = (step) => {
+        switch (step) {
+            case 'none':
+                const element = document.getElementById(id);
+                if (element != null) element.remove();
+                break;
+            case 'prompt':
+                const offerPrompt = TikiSdk.UI.Prompt.create(
+                    TikiSdk.config()._offers[0],
+                    () => {
+                        offerPrompt.remove();
+                        TikiSdkAnon.goTo('terms');
+                    },
+                    () => {
+                        TikiSdkAnon.setDecisionCookie(false);
+                        offerPrompt.remove();
+                    },
+                    () => {
+                        offerPrompt.remove();
+                        TikiSdkAnon.goTo('learnMore');
+                    },
+                    TikiSdk.config().activeTheme
+                );
+                TikiSdkAnon.showScreen(offerPrompt);
+                break;
+            case 'learnMore': {
+                const learnMore = TikiSdk.UI.LearnMore.create(() => {
+                    learnMore.remove();
+                    TikiSdkAnon.goTo('prompt');
+                }, TikiSdk.config().activeTheme);
+                TikiSdkAnon.showScreen(learnMore);
+                break;
+            }
+            case 'terms': {
+                const terms = TikiSdk.UI.Terms.create(
+                    {
+                        src: TikiSdk.config()._offers[0]._terms,
+                    },
+                    async () => {
+                        TikiSdkAnon.setDecisionCookie(true);
+                        terms.remove();
+                    },
+                    () => {
+                        terms.remove();
+                        TikiSdkAnon.goTo('prompt');
+                    },
+                    TikiSdk.config().activeTheme
+                );
+                TikiSdkAnon.showScreen(terms);
+                break;
+            }
+        }
+    }
+
+    static showScreen = (screen) => {
+        const div = document.getElementById(TikiSdkAnon.id);
+        if (div != null) {
+            div.appendChild(screen);
+        }
+    }
+
+    static createOverlay = () => {
+        const overlay = TikiSdk.UI.Overlay.create(() => TikiSdkAnon.goTo('none'));
+        overlay.id = TikiSdkAnon.overlayId;
+        return overlay;
+    }
+
+    static setDecision = (accept) => {
+        let expire = new Date()
+        let expireTime = expire.setFullYear(expire.getFullYear() + 1)
+        expire.setTime(expireTime)
+        if (accept) {
+            TikiCookiePrivacy.handleAccept()
+        } else {
+            TikiCookiePrivacy.handleDecline()
+        }
+    }
+}
