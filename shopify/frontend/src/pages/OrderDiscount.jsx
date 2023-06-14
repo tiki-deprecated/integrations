cd031b2b50d9990bce5bda4314b7c6bff8065e97
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-06-14 08:34:19 -0300

    fix(shopify): order discount page

--- shopify/frontend/src/pages/OrderDiscount.jsx
+++ shopify/frontend/src/pages/OrderDiscount.jsx
@@ -5,17 +5,18 @@ import { MinimumRequirements } from "../groups/MinimumRequirements";
 import { PurchaseType } from "../groups/PurchaseType";
 import {Page, VerticalStack, Divider} from '@shopify/polaris';
 
+const saveDiscountData = () => {
+    console.log("savedata")
+}
+
 export function OrderDiscountPage({ smUp }) {
     return <Page
     divider
-    primaryAction={{ content: "View on your store", disabled: true }}
-    secondaryActions={[
-    {
-        content: "Duplicate",
-        accessibilityLabel: "Secondary action label",
-        onAction: () => alert("Duplicate action"),
-    },
-    ]}>
+    primaryAction={{ 
+        content: "Save",
+        onAction: saveDiscountData
+    }}
+    >
     <VerticalStack gap={{ xs: "8", sm: "4" }}>
         < DiscountAmount />
         {smUp ? <Divider /> : null}
